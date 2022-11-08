ff2468f923d87932379d5a91902fddd525e9e1cc
Cihat <copurcihat@gmail.com>
2022-11-08 16:09:02 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import AppRouter from './router/AppRouter';
+
+function App() {
+  return (
+    <center className="App">
+      
+    <AppRouter/>
+    </center>
+  );
+}
+
+export default App;
+
