db9e594c1e478d5e59f362c9d46dc2f9e4df2ce8
Cihat <copurcihat@gmail.com>
2022-11-29 10:59:00 +0300

    3

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,53 +1,42 @@
-import Form from "react-bootstrap/Form"
-import Button from  "react-bootstrap/Button"
-import { useNavigate } from "react-router-dom"
-
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
 
+  const userInfo = {
+    username: "Alex",
+  };
 
-const navigate=useNavigate();
-
-const userInfo={
-  username:"Alex"
-}
-
-const handleSubmit=(e)=>{
-e.preventDefault();
-sessionStorage.setItem("user",JSON.stringify(userInfo))
-navigate(-1);
-
-}
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
 
   return (
     <div className="login">
-      
- <div className="input">
- 
- <Form className="form" onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label></Form.Label>
-        <Form.Control type="text" placeholder="Enter UserName" />
-        <Form.Text className="text-muted">
-          
-        </Form.Text>
-      </Form.Group>
+      <div className="input">
+        <Form className="form" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Enter UserName" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label></Form.Label>
-        <Form.Control type="password" placeholder="Enter Password" />
-      </Form.Group>
-      
-      <Button   variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
- </div>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label></Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
