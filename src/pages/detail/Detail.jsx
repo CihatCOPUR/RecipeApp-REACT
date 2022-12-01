b5dbe4fce44d5616cfe1c977ddc68472c7f861fa
Cihat <copurcihat@gmail.com>
2022-12-01 20:58:51 +0300

    j

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom'
 
 const Detail = () => {
+  const {state}=useLocation();
+  console.log(state)
   return (
-    <div>
-      Detail
-    </div>
+    <center className="detail">
+      <h1>{state.label}</h1>
+      <h4></h4>
+      <p>{state.ingredientLines
+}</p>
+      <img src={state.image} alt="" />
+    </center>
   )
 }
 
