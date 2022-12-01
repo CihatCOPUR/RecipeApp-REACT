c451cdba2a37a8bb69ad7b8603c496a2d6e10f56
Cihat <copurcihat@gmail.com>
2022-12-01 21:22:02 +0300

    6

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h1>About Us</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, placeat iusto ducimus eius omnis ipsum quo veniam obcaecati maxime itaque, veritatis, laborum quaerat natus incidunt inventore expedita ea nihil vitae perferendis eaque perspiciatis? Iusto exercitationem in maxime quos possimus est.</p>
       </div>
-      <div className="aboutImg">
+      <div className="aboutTeam">
         <h1>CODEX <span >TEAM</span></h1>
       </div>
     </center>
