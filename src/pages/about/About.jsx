d5f593a0cfb1464ce90d350486e1f5a2bac4235a
Cihat <copurcihat@gmail.com>
2022-12-01 21:13:16 +0300

    k

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+    <center className="about">
+      <div className="aboutWrite">
+        <h1>About Us</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, placeat iusto ducimus eius omnis ipsum quo veniam obcaecati maxime itaque, veritatis, laborum quaerat natus incidunt inventore expedita ea nihil vitae perferendis eaque perspiciatis? Iusto exercitationem in maxime quos possimus est.</p>
+      </div>
+      <div className="aboutImg">
+        <h1>CODEX <span >TEAM</span></h1>
+      </div>
+    </center>
+
+    </div>
   )
 }
 
